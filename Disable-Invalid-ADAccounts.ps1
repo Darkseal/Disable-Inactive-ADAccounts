17e3aef56d878d0d8cd611789e28a8d0c7447c91
Darkseal <games@myst.it>
2019-08-22 03:32:25 +0200

    Initial commit.

---
+++ Disable-Invalid-ADAccounts.ps1
@@ -0,0 +1,99 @@
+﻿#########################################################
+#
+# ---------------------------------------------
+# Disable-Inactive-ADAccounts
+# ---------------------------------------------
+# A small Powershell script that disables all the Active Directory user accounts inactive for more than X days
+# (and/or deletes those that have been disabled more than Y days ago).
+#
+# by Darkseal - https://www.ryadel.com/
+#
+# Licensed under GNU - General Public License, v3.0
+# https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+# Fork me on GitHub!
+# https://github.com/Darkseal/Disable-Inactive-ADAccounts
+#
+#########################################################
+
+# - days : disable all inactive accounts for X days or more.
+#          If set is -1, it won't disable anything. Default is 180.
+param(
+[Parameter(Mandatory=$false)] [int] $days = 180,
+
+# -deleteDays : delete all previously disabled users since X days or more.
+#               If set to -1, it won't delete anything. Default is -1.
+[Parameter(Mandatory=$false)] [int] $deleteDays = -1,
+
+# -logFile : if set to a valid PATH, it will output a CSV files containing the disabled users.
+#            If set to null (default) it won't create a logFile.
+[Parameter(Mandatory=$false)] [string] $logFile = $null,
+
+# -deleteLogFile : if set to a valid PATH, it will output a CSV files containing the deleted users.
+#            If set to null (default) it won't create a logFile.
+[Parameter(Mandatory=$false)] [string] $deleteLogFile = $null
+)
+
+
+#Search all inactive accounts
+[Datetime]$ts = (Get-Date).AddDays(-$days)
+# (Search-ADAccount -UsersOnly -AccountInactive -TimeSpan $ts.Day | Select-Object DistinguishedName).count
+# Search-ADAccount -UsersOnly -AccountInactive -TimeSpan $ts.Day | Select-Object DistinguishedName,sAMAccountName
+# $users = {(Search-ADAccount -UsersOnly -AccountInactive | ? { $_.LastLogonDate -lt (get-date).AddDays(-$days)}) | Select-Object DistinguishedName,sAMAccountName,Enabled }
+$users = Search-ADAccount -UsersOnly -AccountInactive -TimeSpan $ts.Day | Select-Object DistinguishedName,sAMAccountName, Enabled
+Write-Host "$($users.Count)"
+
+# Adding the time of account disabled in the extentionAttribute10 for all accounts. 
+# This will help while deleting the accounts if [deleteDays] parameter is set.
+[string]$t = ((Get-Date).Datetime)
+$disabled = 0;
+$alreadyDisabled = 0;
+$deleted = 0;
+
+# Disable the inactive accounts found
+if ($days -gt -1) {
+  Foreach ($user in $users)
+  {
+    If ($user.Enabled) {
+      $disabled++
+      Set-ADUser $user.DistinguishedName -add @{extensionAttribute10="$t"}
+      Disable-ADAccount -Identity $user.DistinguishedName
+    }
+    Else {
+      $alreadyDisabled++
+    }
+  }
+  # Export the disabled accounts
+  If ($logFile) { $users | Export-Csv "$logFile" }
+}
+
+
+# if $deleteDays is set, delete the already-disabled accounts found
+If ($deleteDays -gt -1) {
+  $tsD = (Get-Date).AddDays(-$deleteDays)
+  $usersD = Get-ADUser -Filter {enabled -eq $false} -Properties * | Where-Object{"extensionAttribute10" -in $_.PSobject.Properties.Name} | Where-Object{$tsD -ge $_.extensionAttribute10}
+  Foreach($user in $usersD) {
+    $deleted++
+    Remove-ADUser $user
+  }
+
+  # Export the deleted accounts
+  If ($deletedLogFile) { $usersD | Export-Csv "$deletedLogFile" }
+}
+
+
+
+Write-Host ""
+Write-Host "Operation complete:"
+If ($days -gt -1) {
+  Write-Host " - $($disabled) accounts has been disabled ($($alreadyDisabled) already disabled)."
+  If ($logFile) { 
+    Write-Host "   Disabled accounts logged to $($logFile) file."
+  }
+}
+If ($deleteDays -gt -1) {
+  Write-Host " - $($deleted) accounts has been deleted."
+  If ($deleteLogFile) { 
+    Write-Host "   Deleted accounts logged to $($deleteLogFile) file."
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # Disable-Inactive-ADAccounts
-A small Powershell script that disables all the Active Directory user accounts inactive for more than N days
+A small Powershell script that disables all the Active Directory user accounts inactive for more than N days (and/or deletes those that have been disabled more than Y days ago).
+
+## Usage
+To disable all AD users that has been inactive for 180 days or more (without deleting them):
+
+    > powershell .\Disable-Invalid-ADAccounts.ps1 -days 180
+
+Same thing as before, plus creating a `logFile.csv` file containing a list of all disabled users:
+
+    > powershell .\Disable-Invalid-ADAccounts.ps1 -days 180
+
+To disable all AD users that has been inactive for 180 days or more and also delete those that have been previously disabled more than 180 days ago.
+
+    > powershell .\Disable-Invalid-ADAccounts.ps1 -days 180 -deleteDays 180
+
+Same thing as before, plus creating a `logFile.csv` file containing a list of all disabled users and a `deleteLogFile.csv` file containing a list of all deleted users:
+
+    > powershell .\Disable-Invalid-ADAccounts.ps1 -days 180
+
+## License
+Licensed under GNU - General Public License, v3.0 - https://www.gnu.org/licenses/gpl-3.0.en.html
+
+## Credits
+ - Darkseal/Ryadel, https://www.ryadel.com/
+
+
+
+-deleteDays 0 -deleteLogFile deleteLog.csv
+
+## License
+Licensed under GNU - General Public License, v3.0 - https://www.gnu.org/licenses/gpl-3.0.en.html
+
+## Credits
+ - Darkseal, https://www.ryadel.com/
